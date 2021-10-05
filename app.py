a2cf9d0a61745850fd04af2f1e9c9ac3dbff2b33
jal347 <linjason.03@gmail.com>
2021-10-04 21:12:06 -0700

    fixing stuff

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from opensearchpy import OpenSearch
-from flask import Flask, request, url_for, render_template, redirect
+from opensearchpy import OpenSearch, exceptions
+from flask import Flask, request, Response, url_for, render_template, redirect
 import requests
 import features
-import logging
 import json
 from utils import set_key
 
@@ -11,41 +10,44 @@ app.config['JSON_SORT_KEYS'] = False
 client = OpenSearch("search-gene-y5n3e4j4sgnn7rigtjlod6hboq.us-east-2.es.amazonaws.com:80")
 
 
+@app.route('/gene/')
 @app.route('/gene/<gene_id>')
-def annotation(gene_id):
+def annotation(gene_id=None):
 
-    # search_param = {
-    #     "query": {
-    #         "terms": {
-    #             "_id": [gene_id]
-    #         }
-    #     }
-    # }
-    #
-    # response = client.search(index="mygene", body=search_param)
-    #
-    # if len(response["hits"]["hits"][0]):
-    #     return response
-    # else:
-    #     return 404
+    # checks if id exists if not return error
+    if gene_id is None:
+        doc = {
+            "code": 400,
+            "success": False,
+            "error": "Bad Request",
+            "missing": "id"
+        }
+        return doc, 400
 
-    response = requests.get("https://search-gene-y5n3e4j4sgnn7rigtjlod6hboq.us-east-2.es.amazonaws.com/mygene/_doc/" +
-                            gene_id)
-    response = response.json()
+    fields = None
+    args = request.args
+    if "fields" in args:
+        fields = args["fields"].split(",")
 
-    #if we can access a document with the id
-    if response["found"]:
-        doc = {}
-        set_key(doc, "_id", response["_id"])
-        set_key(doc, "_version", response["_version"])
-        doc.update(response["_source"])
-    else:
+    # error handing exception to see if id is valid
+    try:
+        response = client.get(index="mygene", id=gene_id, _source_includes=fields)
+    except exceptions.NotFoundError:
         doc = {
             "code": 404,
-            "success": bool(False),
-            "error": "Gene ID " + "'" + gene_id + "' not found"
+            "success": False,
+            "error": "Gene ID '" + gene_id + "' not found"
         }
+        return doc, 404
+
+    doc = {
+        "_id": response["_id"],
+        "_version": response["_version"]
+    }
+    doc.update(response["_source"])
+
     return doc
+
 
 
 # this route will only work if it has a q or aggs field
@@ -55,11 +57,11 @@ def query():
     args = request.args
     size = 10
     start = 0
-
+    fields = None
+    search_param = {}
     # q field
     if "q" in args:
         q = args["q"]
-
         # make a query with the q field
         search_param = {
             "query": {
@@ -72,13 +74,15 @@ def query():
     # if aggs field
     if "aggs" in args:
         field = args["aggs"]
-        search_param = {
+        search_param_2 = {
             "aggs": {
                 "facets": {
                     "terms": {"field": field}
                 }
             }
         }
+        search_param.update(search_param_2)
+
 
 
     # checks for size parameter (size is defaulted at 10)
@@ -92,16 +96,17 @@ def query():
     if "fields" in args:
         # get the fields in a list
         fields = args["fields"].split(",")
-        response = client.search(index="mygene", body=search_param, size=size, _source_includes=fields, from_=start)
-    else:
-        response = client.search(index="mygene", body=search_param, size=size, from_=start)
+
+    response = client.search(index="mygene", body=search_param, size=size, _source_includes=fields, from_=start)
 
 
     # make the response look like mygene.info
-    doc = {}
-    set_key(doc, "took", response["took"])
-    set_key(doc, "total", response["hits"]["total"]["value"])
-    set_key(doc, "max_score", response["hits"]["max_score"])
+    doc = {
+        "took": response["took"],
+        "total": response["hits"]["total"]["value"],
+        "max_score": response["hits"]["max_score"]
+    }
+
     if "aggs" in args:
         aggregation = {
             "facets": {
@@ -127,7 +132,7 @@ def query():
     # check if pretty print is true defaulted false
     if "pretty" in args:
         if args["pretty"].lower() == "true":
-            app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+            return Response(json.dumps(doc, indent=4), headers={"Content-Type": "application/json"})
 
     return doc
 
